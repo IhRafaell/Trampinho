ef3c71134c869889c08d82133479282a37fb1e03
IhRafaell <135737305+IhRafaell@users.noreply.github.com>
2024-12-07 16:56:07 -0300

    expanded funcionando

--- users-prestador.js
+++ users-prestador.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "",
     database: "Trampinho",
 });
 
